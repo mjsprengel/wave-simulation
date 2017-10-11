31b3e7647221af9ee2ab92e855ebe58f0ecc5e99
mjsprengel <mjsprengel713@gmail.com>
2017-10-11 00:14:42 -0500

    Update phys_sketch.js

--- phys_sketch.js
+++ phys_sketch.js
@@ -20,7 +20,7 @@ function draw(){
     var sig = height/12;
     var Tsig = sin(frameCount*0.092);
 
-    var w = map(width*sin(frameCount*.01)**2, 0, displayWidth, 75, displayWidth/2);
+    var w = map(width*sin(frameCount*.01)**2, 0, displayWidth, 75, displayWidth/3);
     //var w = width/6;
     bubbles.push(new Bubble(width/2, height/2 + amplitude*T + sig*Tsig, 0, 255*Tsig));
 
@@ -33,5 +33,3 @@ function draw(){
     }
 }
 
-function mouseClicked(){
-}
